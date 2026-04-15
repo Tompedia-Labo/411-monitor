0b235254044f10be55db37a495cfa74aa7c55900
run01ia <208008422+run01ia@users.noreply.github.com>
2026-04-15 18:29:08 +0900

    人物検知とmain追加

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
+from config import Config
 from api.discord_webhook import send_discord_message
+from person_detect.detector import PersonDetector
+from person_detect.rtsp_handler import RTSPStream
+from person_detect.utils import draw_boxes
+import cv2
 
 def main():
-    print(send_discord_message("Hello from Python"))
+    stream = RTSPStream(Config.CAMERAS[0])
+    detector = PersonDetector()
+    count = 0
+    try:
+        for frame in stream.frames():
+            boxes = detector.detect(frame)
+            frame = draw_boxes(frame, boxes)
+            if count != len(boxes):
+                count = len(boxes)
+                print(send_discord_message(f"411には{count}人います！"))
+            
+            cv2.putText(frame, f"People: {count}", (10,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+            cv2.imshow("CamCount", frame)
 
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    finally:
+        stream.release()
+        cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
--- person_detect/rtsp_handler.py
+++ person_detect/rtsp_handler.py
@@ -3,15 +3,18 @@ from models import CameraAuth
 
 class RTSPStream:
     def __init__(self, CameraAuth):
-        self.cap = cv2.VideoCapture(CameraAuth.url)
-
+        print(CameraAuth.user!="" and CameraAuth.password!="")
         if CameraAuth.user!="" and CameraAuth.password!="":
+            print("!!!!!!!!!!!!")
             self.url_with_auth = f"rtsp://{CameraAuth.user}:{CameraAuth.password}@{CameraAuth.url}"
         else:
             self.url_with_auth = f"rtsp://{CameraAuth.url}"
 
+        self.cap = cv2.VideoCapture(self.url_with_auth)
+
         if not self.cap.isOpened():
-            raise ValueError(f"Couldn't open RTSP stream!: {CameraAuth.url}")
+            raise ValueError(f"Couldn't open RTSP stream!: {self.url_with_auth}")
+
 
     def frames(self):
         while True:
